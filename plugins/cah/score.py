0913a4e91b96d6ac21341531db2a099851990f6e
Abra <a@abra.me>
2015-06-29 23:01:36 +0400

    zwsp

--- plugins/cah/score.py
+++ plugins/cah/score.py
@@ -22,8 +22,11 @@ class Scores(object):
         r.append(i)
     return r
 
+  def inject_zwsp(self, nick):
+    return nick[:1] + '\u200b' + nick[1:]
+
   def __str__(self):
     s = []
     for i, j in sorted(self.scores.items(), key=lambda x: -x[1]):
-      s.append('{}-{}p'.format(i, j))
+      s.append('{}-{}p'.format(self.inject_zwsp(i), j))
     return 'Scores: {}.'.format(', '.join(s))
