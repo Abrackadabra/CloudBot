25fcc0f816299551d1c74e8f4b4408edabd9e821
Abra <a@abra.me>
2015-08-25 16:41:35 +0400

    fix deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,2 +1,3 @@
 chmod 600 travis/yacah_deploy_key
-ssh -o StrictHostKeyChecking=no -i travis/yacah_deploy_key yacahb@abra.me 'sudo restart yacah'
+ssh -o StrictHostKeyChecking=no -i travis/yacah_deploy_key yacahb@abra.me \
+  'cd yacahb; git pull; sudo restart yacah'
